345ff979af293d845539dd72a277a4ffc1f38067
patolesahil5 <71871488+patolesahil5@users.noreply.github.com>
2024-05-16 11:08:59 +0000

    Strings

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,32 @@
+// const name = 'Sahil';
+// const repoCount = 10;
+
+// string interpolation - `` which can also include other objects.
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('hitesh-Hc');
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.indexOf('t'));
+// console.log(gameName.toLowerCase());
+
+// const newString = gameName.substring(0, 4);
+// // console.log(newString);
+
+// const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+// const newStringOne = "    hitesh    ";
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url = "https://www.google.com/google%20@yts"
+
+// console.log(url.replace('%20', '-'));
+
+// console.log(url.includes('google')); 
+
+console.log(gameName.split('-'));
