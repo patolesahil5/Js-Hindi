5e6febdffea99dfe96293b9bfe916ccbc60e2c66
patolesahil5 <71871488+patolesahil5@users.noreply.github.com>
2024-05-16 08:06:08 +0000

    conversion & Operation done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,6 +22,32 @@
 "sahil" => true
 */
 
-let someNumber = null
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// let someNumber = null
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
+
+// **************Operations*********************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// let str1 = "hello"
+// let str2 = "sahil"
+
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+"");
+
+// let num1, num2, num3
+// num1=num2=num3=2+2;
+// console.log(num3);
+
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter);
