5912fddfa4b1504418e20a7f4bd6ac7e4f0e9245
patolesahil5 <71871488+patolesahil5@users.noreply.github.com>
2024-05-16 07:34:52 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+// let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN - Not a Number
+// "true" => 1; false => 0
+
+// let isLoggedIn = "0";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+/*
+1 => true; 0 => false
+"" => false
+"sahil" => true
+*/
+
+let someNumber = null
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
