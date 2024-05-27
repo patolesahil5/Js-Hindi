aa32d2067a36542f2fbc9e9e1f36993e0ba14091
patolesahil5 <71871488+patolesahil5@users.noreply.github.com>
2024-05-27 06:16:43 +0000

    String Practice & 03_Objects work  done

---
+++ 01_basics/string_practice.js
@@ -0,0 +1,17 @@
+let string_val = "Hypothermia";
+let string_val2 = "is a disease";
+
+// console.log(string_val.charAt(2));
+// console.log(string_val.charCodeAt(1));
+// console.log(string_val.concat(" " + string_val2));
+// console.log(string_val.indexOf("H"));
+// console.log(string_val.lastIndexOf("y"));
+console.log(string_val2.match("ase"));
+console.log(string_val2.replace("disease", "worst disease"));
+console.log(string_val.search("ther"));
+console.log(string_val2.slice(8, 12));
+console.log(string_val.split());
+console.log(string_val.substring(0,4));
+console.log(string_val.toLowerCase());
+console.log(string_val.toUpperCase());
+console.log(string_val2.valueOf());
---
+++ 02_basics/03_objects.js
@@ -0,0 +1,42 @@
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isloggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullName: {
+        userFullName: {
+            firstName: "Sahil",
+            lastName: "Patole"
+        }
+    }
+}
+
+// console.log(regularUser.fullName.userFullName.firstName);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+const obj3 = Object.assign({}, obj1, obj2, obj4)
+// console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "s@gmail.com"
+    }
+]
+
+// users[1].email
+// console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isloggedIn'));
