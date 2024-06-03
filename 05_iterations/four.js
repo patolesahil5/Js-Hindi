17194240de268dcd0e2cc6234de0cb9bd97c331b
patolesahil5 <71871488+patolesahil5@users.noreply.github.com>
2024-06-03 12:23:23 +0000

    Maps, ForEach, ForOF, ForIN Completed

---
+++ 05_iterations/four.js
@@ -0,0 +1,28 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: 'Ruby',
+    swift: 'Swift by Apple'
+}
+
+
+// for (const key in myObject) {
+//     console.log(myObject[key]);
+//     console.log(key);
+//     console.log(`${key} shortcut is for ${myObject[key]}`);
+// }
+
+const programming = ["js", "ruby", "java", "cpp"];
+
+// for (const key in programming) {
+//     console.log(programming[key]);
+// }
+
+// const map = new Map()
+// map.set('IN', 'India')
+// map.set('USA', 'United States of America')
+// map.set('Fr', 'France')
+
+// for (const key in map) {
+//     console.log(key);
+// }
