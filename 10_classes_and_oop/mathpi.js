2975d72c9ab79eef4d62630bf295704407c1673b
patolesahil5 <71871488+patolesahil5@users.noreply.github.com>
2024-06-24 11:26:41 +0000

    Object Descriptor Completed

---
+++ 10_classes_and_oop/mathpi.js
@@ -0,0 +1,31 @@
+const descriptor = Object.getOwnPropertyDescriptor(Math, "PI");
+
+// console.log(Math.PI);
+// Math.PI = 5;
+// console.log(Math.PI);
+// console.log(descriptor);
+
+const chai = {
+    name: "ginger",
+    price: "250",
+    isAvailable: true,
+
+    orderChai: function(){
+            console.log("Chai nahi bani");
+    }
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+Object.defineProperty(chai, "name", {
+    writable: false,
+    enumerable: false
+})
+
+// console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+for (let [key, value] of Object.entries(chai)) {
+    if(typeof value !== 'function'){
+        console.log(`${key} : ${value}`);
+    }
+}
